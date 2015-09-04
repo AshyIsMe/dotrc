6839e33398f43139b332f27799f36d7fb291b75e
Aaron Ash <aaron.ash@gmail.com>
2015-09-04 11:41:10 +0800

    Updated template.sh script to use getargs

--- template.sh
+++ template.sh
@@ -4,9 +4,9 @@ set -e
 ## This is a template bash script with usage docs and arg parsing
 ## Usage: template.sh [options]
 ##
-##       [-h | --help]     Print this help message
-##       [-f | --foo]      Foos the bar
-##       [-b | --bar]      Bars the baz
+##       [-h     | --help]     Print this help message
+##       [-f arg | --foo arg]  Foos the bar
+##       [-b arg | --bar arg]  Bars the baz
 
 BASE=$(cd $(dirname $0); pwd -P)                                        
 
@@ -22,29 +22,38 @@ error() {
 
 [[ $# == 0 ]] && usage
 
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--help") set -- "$@" "-h" ;;
+    "--foo")  set -- "$@" "-f" ;;
+    "--bar")  set -- "$@" "-b" ;;
+    *)        set -- "$@" "$arg"
+  esac
+done
+# Parse short options
+OPTIND=1
+while getopts "hf:b:" opt
+do
+  case "$opt" in
+    "h") usage; exit 0 ;;
+    "f") FOO="$OPTARG" ;;
+    "b") BAR="$OPTARG" ;;
+    "?") usage >&2; exit 1 ;;
+    ":") error "Option -$OPTARG requires an argument.";;
+  esac
+done
+shift $(expr $OPTIND - 1) # remove options from positional parameters
+
+
 ARGS=$@
 
-while [[ $# > 0 ]]; do
-   key="$1"
-   case $key in
-      -h|--help)
-         usage
-         ;;
-      -f|--foo)
-         FOO=1
-         ;;
-      -b|--bar)
-         BAR=1
-         ;;
-   esac
-   shift
-done
-
 if [[ -n "$FOO" ]]; then
-   echo "FOO"
+   echo "FOO: $FOO"
 fi
 if [[ -n "$BAR" ]]; then
-   echo "BAR"
+   echo "BAR: $BAR"
 fi
 
-echo "full args were: $ARGS"
+echo "Rest of the args were: $ARGS"
