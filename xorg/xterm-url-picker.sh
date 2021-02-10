bfa048082184af1b0392fa397f5dc6a671ff2326
Aaron Ash <aaron.ash@gmail.com>
2021-02-10 16:52:14 +1000

    xterm and doom-emacs updates

--- xorg/xterm-url-picker.sh
+++ xorg/xterm-url-picker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # Cut off the w3c header urls with sed
-xdg-open $(sed 1,5d ~/xterm.*.xhtml | grep -hoE '(http|https)://[a-zA-Z0-9./?=_%:-]*' | rofi -dmenu)
+# xdg-open $(sed 1,5d ~/xterm.*.xhtml | grep -hoE '(http|https)://[a-zA-Z0-9./?=_%:-]*' | rofi -dmenu)
+xdg-open $(sed 1,5d ~/xterm.*.xhtml | grep -hoE '(http|https)://[a-zA-Z0-9./?=_%:-]*' | sort -u | rofi -dmenu)
 rm ~/xterm.*.xhtml
